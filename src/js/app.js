b5b062fb9fafe5bf40b71b883ef26c77f5f4c2ac
KamilStaszewski <kamilstaszewski95@gmail.com>
2018-08-16 12:55:01 +0200

    started parallax, fixed font-sizes

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,13 @@
-console.log('cos')
+document.addEventListener('DOMContentLoaded', function() {
+
+    function parallax() {
+        const parallax = document.querySelector('.parallax');
+    
+        //przy scrollu przewijam lekko tlo
+        window.addEventListener('scroll', function() {
+            parallax.style.backgroundPositionY = -(this.scrollY * 0.2) + 'px';
+        });
+    }
+
+
+});
